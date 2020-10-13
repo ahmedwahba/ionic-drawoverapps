c0bc0977cb7a957559491c574aff46b1af54628f
Ahmed Wahba <>
2020-10-13 16:36:46 +0200

    add test over app

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -15,4 +15,26 @@ export class FolderPage implements OnInit {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
   }
 
+  onClick() {
+    (<any>window).overApps.checkPermission(function(msg){
+          console.log(msg);
+          var options = {
+            path: "test.html",          // file path to display as view content.
+            hasHead: true,              // display over app head image which open the view up on click.
+            dragToSide: false,          // enable auto move of head to screen side after dragging stop. 
+            enableBackBtn: false,       // enable hardware back button to close view.
+            enableCloseBtn: true,      //  whether to show native close btn or to hide it.
+            verticalPosition: "top",    // set vertical alignment of view.
+            horizontalPosition: "left"  // set horizontal alignment of view. 
+      };
+      
+      (<any>window).overApps.startOverApp(options,function (success){
+            console.log(success);
+      },function (err){
+            console.log(err);
+      });
+    });
+   
+  }
+
 }
